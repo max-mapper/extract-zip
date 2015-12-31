6e00ff63d6649421be785167eefaf3d393836928
Josh Wolfe <thejoshwolfe@gmail.com>
2015-12-31 13:27:54 -0700

    update to yauzl 2.4 api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var fs = require('fs')
 var path = require('path')
-var async = require('async')
 var yauzl = require('yauzl')
 var mkdirp = require('mkdirp')
 var concat = require('concat-stream')
@@ -17,19 +16,18 @@ module.exports = function (zipPath, opts, cb) {
   function openZip () {
     debug('opening', zipPath, 'with opts', opts)
 
-    yauzl.open(zipPath, {autoClose: false}, function (err, zipfile) {
+    yauzl.open(zipPath, {lazyEntries: true}, function (err, zipfile) {
       if (err) return cb(err)
 
       var cancelled = false
       var finished = false
 
-      var q = async.queue(extractEntry, 1)
+      zipfile.readEntry()
 
-      q.drain = function () {
-        if (!finished) return
+      zipfile.on('close', function () {
         debug('zip extraction complete')
         cb()
-      }
+      })
 
       zipfile.on('entry', function (entry) {
         if (cancelled) {
@@ -41,23 +39,20 @@ module.exports = function (zipPath, opts, cb) {
 
         if (/^__MACOSX\//.test(entry.fileName)) {
           // dir name starts with __MACOSX/
+          zipfile.readEntry()
           return
         }
 
-        q.push(entry, function (err) {
+        extractEntry(entry, function (err) {
           // if any extraction fails then abort everything
           if (err) {
             cancelled = true
-            q.kill()
             zipfile.close()
             return cb(err)
           }
           debug('finished processing', entry.fileName)
+          zipfile.readEntry()
         })
-      })
-
-      zipfile.on('end', function () {
-        finished = true
       })
 
       function extractEntry (entry, done) {
