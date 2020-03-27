eed71b0a7dffd0df020b4b68c5c18da0864fa38c
Mark Lee <electronjs@lazymalevolence.com>
2020-03-27 15:47:27 -0700

    feat: add TypeScript definition

---
+++ index.test-d.ts
@@ -0,0 +1,26 @@
+import { Entry, ZipFile } from 'yauzl';
+import * as extract from '.';
+
+const zip = '/path/to/zip';
+const dir = '/path/to/dir';
+const defaultFileMode = 0o600;
+
+let options: extract.Options = {
+  dir,
+};
+options = {
+  dir,
+  defaultDirMode: 0o700,
+  defaultFileMode,
+  onEntry: (entry: Entry, zipfile: ZipFile): void => {
+    console.log(entry);
+    console.log(zipfile);
+  }
+};
+
+try {
+  await extract(zip, options);
+  console.log('done');
+} catch (err) {
+  console.error(err);
+}
